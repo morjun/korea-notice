bd6f57b585e6232092e9f2aaeed83547dbc78ce7
JediMasterJune <anbndn5@gmail.com>
2023-11-12 12:05:58 +0900

    Clean code

--- main.py
+++ main.py
@@ -42,16 +42,12 @@ def dorm_notice_init():
 
 def dorm_notice_init_new():
     dorm_list = {'https://dorm.korea.ac.kr/front/board/1/post': 'dorm-total',}
-                 # 'https://dorm.korea.ac.kr:42305/src/board/list.php?code=notice': 'dorm-oldgik',
-                 # 'https://dorm.korea.ac.kr:42305/src/board/list.php?code=notice1': 'dorm-newgik'}
     for dorm_key in dorm_list.keys():
         dorm_notice_new(dorm_key, dorm_list[dorm_key])
 
 
 def get_post_id_from_javascript(href):
     post_id = int(href.split("'")[1])
-    # if __debug__:
-    #     print(f"post_id: {post_id}")
     return post_id
 
 
@@ -66,8 +62,6 @@ def dorm_notice(url, cat):
 
     for tr in tr_list:  # 각 게시글 당
         td_list = tr.select('td')  # td 리스트 생성
-        # if __debug__:
-        #     print(f'td_list: {td_list}')
         for td in td_list:
             # 글 제목, id, 링크 가져오기
             a = td.find('a')
@@ -97,9 +91,7 @@ def dorm_notice_new(url, cat):
     response = requests.get(url)
     category = {}
 
-    # soup = BeautifulSoup(response.content.decode('euc-kr', 'replace'), "html.parser")
     soup = BeautifulSoup(response.content, "html.parser")
-    # print(soup)
 
     tr_list = soup.select(
         'section > div > article.right-content.content_layout_area')
@@ -141,7 +133,6 @@ def dorm_notice_new(url, cat):
 
 def post_id_validate(category, post):
     cur.execute(f'''SELECT id FROM posts WHERE cat = '{post[3]}' AND id = {post[0]} ''')  # 테이블에서 데이터 선택하기
-    # print(f"SELECT id FROM posts WHERE cat = '{post[3]}' AND id = {post[0]}")
     id = cur.fetchone()
     if id is None:  # 데이터베이스에 id가 없을 경우
         tel_bot(category[post[3]], post[1], post[2], post[4])
@@ -153,7 +144,7 @@ def post_id_validate(category, post):
     return 0
 
 
-def tel_bot(cat, title, link, date):
+async def tel_bot(cat, title, link, date):
     title = title.replace('&', '&amp;')
     title = title.replace('<', '&lt;').replace('>','&gt;')
     text = f"""
@@ -162,40 +153,15 @@ def tel_bot(cat, title, link, date):
 <b>제목</b> {title}
 <b>게시일</b> {date}"""
 
-#     text = f"""
-# *{cat}*
-#
-# *제목* {title}
-# *게시일* {date}"""
-
-    # text = text.replace("-", "\\-") #markdown parsing을 위해
-    # print(text)
-
     is_portal = cat.find('포털')
-    # if __debug__:
-    #     print(is_portal)
-
-
     if is_portal != -1:
         text += f'\n\n<a href="{link}">포털 바로가기</a>'
-        # text += f'\n\n[포털 바로가기]({link})'
     else:
         text += f'\n\n<a href="{link}">게시글 바로가기</a>'
-        # text += f'\n\n[게시글 바로가기]({link})'
-
-
-    # if __debug__:
-    #     bot.sendMessage(chat_id=id, text=text, parse_mode="html")
-    # id_channel = bot.sendMessage(chat_id='@korea_noti_test', text="I'm bot").chat_id  # @korea_noti_test 으로 메세지를 보냅니다.
-    # print(id_channel)
-    # with open('id_test.txt','w') as f:
-    #     f.write(str(id_channel))
-    # else:
 
     while True:
         try:
-            bot.sendMessage(chat_id=id, text=text, parse_mode="html") #제목에 <> 등 들어가면 문제 발생함
-            # bot.sendMessage(chat_id=id, text=text, parse_mode="MarkdownV2") #html보다도 고려해야 할 예외가 많음
+            await bot.sendMessage(chat_id=id, text=text, parse_mode="html") #제목에 <> 등 들어가면 문제 발생함
             break
         except Exception as tel_error:
             print(f'Error: {tel_error}, waiting...')
@@ -301,7 +267,6 @@ def portal():
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'ko-KR,ko;q=0.9',
-        # 'User-Agent': ua.chrome,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
         'Cache-Control': 'max-age=0',
         'Referer': 'https://portal.korea.ac.kr/front/Intro.kpd',
@@ -320,8 +285,7 @@ def portal():
         response = session.get(LOGIN_HOME)
         soup = BeautifulSoup(response.content, 'html.parser')
         span = soup.find('span', {'class': 'input'})
-        # if __debug__:
-        #     print(f"id_key_slot: {span}")
+
         inputs = span.select('input')
 
         login_info = {'direct_div': '', 'pw_pass': '', 'browser': 'chrome'}
@@ -345,13 +309,9 @@ def portal():
             login_request = session.post(LOGIN_URL, headers=headers_portal_login, data=login_info)
 
         for board_key in boards.keys():
-            # cat = f'portal_{board_key}'
-            # board_url = f'{URL_BASE}?kind={str(boards[board_key])}'
             path = f'{PATH_BASE}?kind={str(boards[board_key])}'
             board_url = [URL_BASE_FACE, URL_BASE, path]
             portal_posts_process(board_url, board_key, category, portletIds, session, headers_portal_login)
-        # cat = 'portal_recent'
-        # portal_posts_process(URL_BASE, category, session,headers_portal_login)
 
 
 def portal_posts_process(url, cat, category, portletIds, session, headers):
@@ -361,15 +321,10 @@ def portal_posts_process(url, cat, category, portletIds, session, headers):
     portletId = portletIds[cat]
 
     headers = {
-        # 'Content-Type' : 'application/x-www-form-urlencoded',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'ko-KR,ko;q=0.9',
-        # 'User-Agent': ua.chrome,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-        # 'Cache-Control': 'max-age=0',
-        # 'Referer': 'https://portal.korea.ac.kr/front/Intro.kpd',
-        # 'Origin' : 'https://portal.korea.ac.kr',
         'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -379,10 +334,6 @@ def portal_posts_process(url, cat, category, portletIds, session, headers):
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
     }
-
-    # 바로 GET 꼬라박으면 죽음 (먼저 POST로 쿠키 겟또 해야 함)
-    # if __debug__:
-    #     print(session.cookies)
 
     data = {'portletId': portletId, 'queryYn': 'Y', 'url': url_base, 'path': path, 'compId': 'GROUPWARE',
             'menuCd': '', 'moreYn': 'Y'}
@@ -402,8 +353,6 @@ def portal_posts_process(url, cat, category, portletIds, session, headers):
         if "데이터가 없습니다" in li.text:
             break
         span = li.find('span', {'class': 'txt_right'})  # find: 첫번쨰 occurence만 단일 객체로 반환
-        # span_inside_span = span.find('span')
-        # print(span_inside_span)
         span.span.extract()
         date = str(span.text).strip()
 
@@ -425,35 +374,6 @@ def portal_posts_process(url, cat, category, portletIds, session, headers):
         except NameError:  # post_id를 가져오지 못 했을 때
             pass
 
-    # tr_list = soup.select('#Search > table > tbody > tr')
-    # if __debug__:
-    #     print(f'tr_list: {tr_list}')
-
-    # for tr in tr_list:
-    #     span = tr.find('span')  # find: 첫번쨰 occurence만 단일 객체로 반환
-    #     span.i.extract()  # i 태그 제외
-    #     date = str(span.text)
-    #     date = date.replace('.', '-')
-    #
-    #     a = tr.find('a')
-    #     title = a.text
-    #
-    #     href = a.get('href')
-    #     href_parsed = parse_qsl(href)
-    #     link = (url + href)
-    #
-    #     post_id = int(href_parsed[1][1])
-    #     try:
-    #         if post_id:
-    #             post = [post_id, title, link, cat, date]
-    #             if __debug__:
-    #                 print_info(post)
-    #             post_id = post_id_validate(category, post)
-    #     except NameError:  # post_id를 가져오지 못 했을 때
-    #         pass
-    #
-
-
 def print_info(post):
     print(f'''post_id: {post[0]}
 제목: {post[1]}
@@ -468,21 +388,16 @@ if __name__ == '__main__':
         if FIDDLER:
             proxies = {"http": "http://127.0.0.1:8888", "https": "http:127.0.0.1:8888"}
             verify = r"FiddlerRoot.pem"
-    # ua = UserAgent()
-
-    # DB 연결 및 테이블 생성
     if __debug__:
         conn = sqlite3.connect('notice_test.db')
     else:
         conn = sqlite3.connect('notice.db')
-    # conn.row_factory = lambda cursor, row: row[0]  # fetchall시 튜플로 나오는 현상 방지
     cur = conn.cursor()
     try:
         cur.execute('''CREATE TABLE posts(id INT, title TEXT, link TEXT, cat TEXT, date TEXT, UNIQUE(id, cat))''')
     except:
         pass
 
-    # 텔레그램 봇 초기화
     with open('token.ini', 'r') as f:
         my_token = str(f.read()).strip() # 토큰을 설정해 줍니다.
     bot = telegram.Bot(token=my_token)  # 봇에 연결합니다.
@@ -502,7 +417,6 @@ if __name__ == '__main__':
             portal()
             link = 'https://telegram.org/blog/link-preview#:~:text=Once%20you%20paste%20a%20URL,now%20shown%20for%20most%20websites.'
             link2 = 'https://dorm.korea.ac.kr:42305/src/board/view.php?page=1&code=notice2&mode=&no=40659&s_type=1&s_text='
-            # tel_bot('테스트', '봇 테스트', link2, '2022-02-02')
             display_data = pd.read_sql_query("SELECT * FROM posts", conn)
             print(display_data)
         else:
